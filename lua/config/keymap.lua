791cb35c7eb16703403606c317b86c4fc9fdc31f
wonkhester <kevinradley@outlook.com>
2025-03-22 09:00:35 +0100

    Added telescope

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -1,3 +1,12 @@
 local map = vim.keymap.set
 
 map("n", "<leader>pv", function() vim.cmd.Ex() end)
+
+
+local builtin = require('telescope.builtin')
+
+map('n', '<leader>pf', builtin.find_files, {})
+map('n', '<C-p>', builtin.git_files, {})
+map('n', '<leader>ps', function()
+	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+end)
