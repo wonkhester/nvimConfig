388d3ed57389443b2b52f2546a6fe85531a48a30
wonkhester <kevinradley@outlook.com>
2025-03-22 09:31:44 +0100

    Added harpoon

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -10,3 +10,15 @@ map('n', '<C-p>', builtin.git_files, {})
 map('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
+
+
+local mark = require("harpoon.mark")
+local ui = require("harpoon.ui")
+
+map("n", "<leader>a", mark.add_file)
+map("n", "<C-e>", ui.toggle_quick_menu)
+
+map("n", "<C-h>", function() ui.nav_file(1) end)
+map("n", "<C-t>", function() ui.nav_file(2) end)
+map("n", "<C-n>", function() ui.nav_file(3) end)
+map("n", "<C-s>", function() ui.nav_file(4) end)
