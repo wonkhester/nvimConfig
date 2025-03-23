f84725cb9bd36f8258278385e7ba2ec6e947ae3c
wonkhester <kevinradley@outlook.com>
2025-03-23 17:52:54 +0100

    Added more keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,6 +4,39 @@ map("n", "<leader>pv", function() vim.cmd.Ex() end)
 map("n", "<leader>u", function() vim.cmd.UndotreeToggle() end)
 map("n", "<leader>gs", function() vim.cmd.Git() end)
 
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '>-2<CR>gv=gv")
+
+map("n", "J", "mzJ`z")
+map("n", "<C-d>", "<C-d>zz")
+map("n", "<C-u>", "<C-u>zz")
+map("n", "n", "nzzzv")
+map("n", "N", "Nzzzv")
+
+map("x", "<leader>p", "\"_dP")
+
+map("n", "<leader>y", "\"+y")
+map("v", "<leader>y", "\"+y")
+map("n", "<leader>Y", "\"+Y")
+
+map("n", "<leader>d", "\"_d")
+map("v", "<leader>d", "\"_d")
+
+map("n", "Q", "<nop>")
+map("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+map("n", "<leader>f", function()
+    vim.lsp.buf.format()
+end)
+
+map("n", "<C-k>", "<cmd>cnext<CR>zz")
+map("n", "<C-j>", "<cmd>cprev<CR>zz")
+map("n", "<leader>k", "<cmd>lnext<CR>zz")
+map("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+map("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<left><left><left>")
+map("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+
 local builtin = require('telescope.builtin')
 
 map('n', '<leader>pf', builtin.find_files, {})
