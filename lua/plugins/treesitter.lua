fbfe22f839616a8fcaa52a37db3b1e3358296cf3
wonkhester <kevinradley@outlook.com>
2025-03-24 19:34:08 +0100

    Fixed treesitter now downloads all parsers

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,48 +4,35 @@ return {
     dependencies = {
         'nvim-treesitter/playground',
     },
-    opts = {
-        ensure_installed = {
-            -- Scripting Languages
-            'lua', 'python', 'perl', 'ruby', 'bash',
-
-            -- Web Development
-            'javascript', 'typescript', 'html', 'css', 'scss', 'json', 'yaml', 'graphql', 'tsx',
-
-            -- System Programming
-            'c', 'cpp', 'rust', 'go', 'zig',
-
-            -- Backend & Server-side
-            'java', 'kotlin', 'csharp', 'php', 'sql', 'dockerfile',
-
-            -- Functional & Data Science
-            'haskell', 'elixir', 'clojure', 'r', 'julia',
-
-            -- Other Useful Languages
-            'markdown', 'markdown_inline', 'vim', 'vimdoc', 'toml',
-        },
-        highlight = { enable = true },
-        indent = { enable = true },
-        incremental_selection = {
-            enable = true,
-            keymaps = {
-                init_selection = 'gnn',
-                node_incremental = 'grn',
-                scope_incremental = 'grc',
-                node_decremental = 'grm',
-            },
-        },
-        textobjects = {
-            select = {
+    config = function()
+        require('nvim-treesitter.configs').setup {
+            ensure_installed = "all",
+            highlight = { enable = true },
+            sync_install = false,
+            auto_install = true,
+            additional_vim_regex_highlighting = false,
+            indent = { enable = true },
+            incremental_selection = {
                 enable = true,
-                lookahead = true,
                 keymaps = {
-                    ['af'] = '@function.outer',
-                    ['if'] = '@function.inner',
-                    ['ac'] = '@class.outer',
-                    ['ic'] = '@class.inner',
+                    init_selection = 'gnn',
+                    node_incremental = 'grn',
+                    scope_incremental = 'grc',
+                    node_decremental = 'grm',
                 },
             },
-        },
-    }
+            textobjects = {
+                select = {
+                    enable = true,
+                    lookahead = true,
+                    keymaps = {
+                        ['af'] = '@function.outer',
+                        ['if'] = '@function.inner',
+                        ['ac'] = '@class.outer',
+                        ['ic'] = '@class.inner',
+                    },
+                },
+            },
+        }
+    end
 }
