11d60c18d65a576ed28c05bcd4d2292e527dc296
ntijoh-kevin-radley <kevin.radley@elev.ga.ntig.se>
2025-03-26 10:37:59 +0100

    Added more colorscheme and better way of selecting colorscheme

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -28,15 +28,17 @@ require("config.set")
 require("lazy").setup({
     spec = {
         -- import your plugins
-        { import = "plugins" },
         { import = "plugins.colorschemes" },
         { import = "plugins.lsp" },
+        { import = "plugins" },
     },
     -- Configure any other settings here. See the documentation for more details.
     -- colorscheme that will be used when installing plugins.
-    install = { 
+    install = {
         colorscheme = { "habamax" },
     },
     -- automatically check for plugin updates
     checker = { enabled = true },
 })
+
+require("config.set-after")
---
+++ lua/plugins/colorschemes/gruvbox-material.lua
@@ -0,0 +1,9 @@
+return {
+    "sainnhe/gruvbox-material",
+    name = "gruvbox-material",
+    config = function()
+        vim.g.gruvbox_material_background = "medium" -- Options: 'soft', 'medium', 'hard'
+        vim.g.gruvbox_material_enable_italic = 1
+        vim.g.gruvbox_material_better_performance = 1
+    end,
+}
